7df9044e7e46b1e314c1fee29f6eaedede8c3671
TheProperGlitch <114691366+TheProperGlitch@users.noreply.github.com>
2023-10-22 12:16:09 -0700

    Furnished
    I think...

--- Fleet.java
+++ Fleet.java
@@ -4,6 +4,11 @@ class Fleet {
         myCars = _myCars.clone();
     }
     public Car findMax(){
+        /*
+         A method that takes the form of Fleet.findMax(). It goes through every car in it,
+         finds the one with the most miles, and returns it.
+         
+        */
         Car largest = myCars[0];
         for (int i = 0; i < myCars.length; i++){
             if (myCars[i].getMiles() > largest.getMiles()) largest = myCars[i];
